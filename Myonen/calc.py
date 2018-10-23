85b6d7e578dd5d52b198c96643d8408fa87ae8f0
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2018-10-23 19:08:05 +0200

    das git beschwert sich, wenn ich das nicht mitpushe

--- Myonen/calc.py
+++ Myonen/calc.py
@@ -9,5 +9,11 @@ from pylab import figure, axes, pie, title, show, array
 #N=np.sqrt() print(N)
 #N=np.sqrt() print(N)
 
+
 NS=TS*(NG + np.sqrt(NG)/TG
 print(NS)
+
+#e=1.8932e-06
+#t=2.199e-06
+#f=(e-t)/t*100
+#print(f)

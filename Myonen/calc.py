f5ad96b3a6a297033e3285055f01b8635c02d794
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2018-10-24 08:05:56 +0200

    ...

--- Myonen/calc.py
+++ Myonen/calc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import sympy
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from pylab import figure, axes, pie, title, show, array
@@ -9,11 +10,27 @@ from pylab import figure, axes, pie, title, show, array
 #N=np.sqrt() print(N)
 #N=np.sqrt() print(N)
 
+N=1.8888408187545629e-06
+dN=1.59801780233e-08
 
-NS=TS*(NG + np.sqrt(NG)/TG
-print(NS)
+P=2.71828182845905**(-N)*N
+dP=sp.sqrt( (1.0*2.71828182845905**(-N)*N + 2.71828182845905**(-N))**2 * dN**2 )
 
+C=509
+#print(P)
+#print(dP)
+
+#
+#P = N*np.e**(-N)
+#print(P)
+#
+#print(P.diff(N))
 #e=1.8932e-06
 #t=2.199e-06
 #f=(e-t)/t*100
 #print(f)
+
+U=P*N/C
+print(U)
+dUP=sp.sqrt( (N/C)**2*dP**2 + (P/C)**2*dN**2)
+print(dUP)

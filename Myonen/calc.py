efcbfe5cb111e7ffcb22c78be140ca34824781a4
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2018-10-24 10:04:36 +0200

    Untergrundrate fertig. Fehler sind größer als die Werte selbst. WTF. Muss noch korrigiert werden. Poisson war wohl doch nicht richtig

--- Myonen/calc.py
+++ Myonen/calc.py
@@ -7,30 +7,42 @@ from pylab import figure, axes, pie, title, show, array
 
 #N=np.sqrt() print(N)
 
-#N=np.sqrt() print(N)
-#N=np.sqrt() print(N)
 
-N=1.8888408187545629e-06
-dN=1.59801780233e-08
+#NS=1397196
+#TS=10e-6
+#TG=73966
+#C=508
+#
+#N=NS*TS/TG
+#dN=sp.sqrt(NS)*TS/TG
+#
+N=0.00018889706081172433
+#dN=1.5980727043e-07
+#N = sympy.var('N')
 
-P=2.71828182845905**(-N)*N
-dP=sp.sqrt( (1.0*2.71828182845905**(-N)*N + 2.71828182845905**(-N))**2 * dN**2 )
+#P=N*np.e**(-N)
+#dP=np.sqrt( (-1.0*2.71828182845905**(-N)*N + 2.71828182845905**(-N))**2 * dN**2 )
 
-C=509
-#print(P)
+P=0.000188861382082
+#dP=1.59746904735e-07
+
+#U=P*NS/C
+#dUP=U.diff(P)
+#print(dUP)
+#dUNS=U.diff(NS)
+
+U=0.5194416685028387
+#dP=P.diff(N)
 #print(dP)
 
-#
-#P = N*np.e**(-N)
-#print(P)
-#
-#print(P.diff(N))
-#e=1.8932e-06
-#t=2.199e-06
-#f=(e-t)/t*100
-#print(f)
 
-U=P*N/C
-print(U)
-dUP=sp.sqrt( (N/C)**2*dP**2 + (P/C)**2*dN**2)
-print(dUP)
+print('ab hier neu mit Poisson')
+print('dN')
+dN=np.sqrt(N)
+print(dN)
+print('dP')
+dP=np.sqrt(P)
+print(dP)
+print('dU')
+dU=np.sqrt(U)
+print(dU)

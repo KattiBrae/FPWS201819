d83f289d682a573cef76ffaf33323cdebc624b44
Tobias <janssen-tobi@web.de>
2018-11-21 11:20:22 +0100

    messug

--- HeNeLaser/stabilitätsbedingung.py
+++ HeNeLaser/stabilitätsbedingung.py
@@ -8,6 +8,7 @@ from pylab import figure, axes, pie, title, show
 print('Graphik1')
 print('Steigung','Y-Achsenabschnitt')
 x, y = np.loadtxt('stabilitätsbedingung.txt', unpack=True,delimiter=',')
+y=-y
 
 def f(x,a,b):
     return a*x+b

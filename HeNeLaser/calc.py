9964942baf52f74aac195a69c349f0147f2a94f6
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2018-12-14 14:32:58 +0100

    diskussion finite

--- HeNeLaser/calc.py
+++ HeNeLaser/calc.py
@@ -16,3 +16,14 @@ diffomega=np.sqrt(2)*(-1/2)*b**(-3/2)
 
 domega=np.sqrt( (np.sqrt(2)*(-1/2)*b**(-3/2))**2*db**2 )
 print(domega)
+
+
+print('Abweichung minima')
+#  &\sigma_{\text{max, Fit}} &=&& \SI{50.97 \pm 0.38}{°} 17.71630370806357,   && \SI{230.97 \pm 0.38}{°} 5.641425293328138 \\
+#  &\sigma_{\text{max, Wert}} &=&& \SI{ 60 }{°},                          && \SI{ 244 }{°} \\
+#  &\sigma_{\text{min, Fit}} &=&& \SI{140.97 \pm 0.38}{°} 3.5681350641980574,  && \SI{320.97 \pm 0.38}{°} 2.1902358475869934 \\
+#  &\sigma_{\text{min, Wert}} &=&& \SI{ 146 }{°},                         && \SI{ 328 }{°}. \\
+e=146
+t=320.97
+f=100*(e-t)/t
+print(f)

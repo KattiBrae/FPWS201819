80c361246e37db2df1b7571d592d7069f82ab1e9
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2019-02-06 09:54:04 +0100

    normaler zeeman-effekt

---
+++ Zeeman-Effekt/bilder/rot2/bilder.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+bild = mpimg.imread('1.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('2.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('3.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('4.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('5.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('6.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('7.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('8.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('9.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('10.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('11.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('12.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+plt.show()
+
+
+#70
+#76
+#79
+#81
+#82
+#89
+#88
+#93
+#96
+#102
+#106
+#116

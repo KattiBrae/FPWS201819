07c1d511df7f254dfacae9e0cde750a5a3a1d3bc
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2019-02-14 09:33:55 +0100

    auswertung

---
+++ Zeeman-Effekt/bilder/blau1/bilder.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+bild = mpimg.imread('0.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+plt.show()
+bild = mpimg.imread('1.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('2.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('3.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('4.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('5.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('6.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('7.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('8.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('9.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('10.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+bild = mpimg.imread('11.JPG')
+print(bild.shape)
+plt.gray()
+plt.imshow(bild)
+
+
+
+140
+154
+173
+177
+187
+197
+209
+233
+250
+282
+321
+399
---
+++ Zeeman-Effekt/bilder/tobi/calc.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy as sp
+import sympy
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from pylab import figure, axes, pie, title, show, array
+
+
+#blau1
+414
+326
+268
+264
+224
+208
+195
+171
+185
+173
+160
+123

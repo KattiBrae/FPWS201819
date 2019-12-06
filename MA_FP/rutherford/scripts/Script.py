2c9f25fb2041a373ee907e807bd0687f9f11f237
Lars Kolk <lars.kolk@cern.ch>
2019-12-06 07:15:02 +0100

    ja ich pushe noch ^^

--- MA_FP/rutherford/scripts/Script.py
+++ MA_FP/rutherford/scripts/Script.py
@@ -14,9 +14,10 @@ tau = unp.uarray(432.6,0.6)*365*24*60*60 #s
 t = unp.uarray(7.897,0.013)*10**8 #s
 
 A = func.activity(A_0, tau, t)
-A2 = 15.13
+A2 = 15.64
+A_exp = A2 #*np.pi*101**2
 print("A_theo = ", A)
-print("A_exp = ", A2)
+print("A_exp = ", A_exp)
 #c) Thickness 
 
 def linear(x, a, b):
@@ -35,6 +36,11 @@ plt.plot(p_without, U_without, 'bx', label=r"Wertepaare ohne Folie")
 plt.plot(x_plot, linear(x_plot, *params_w), "-", label = r"Ausgleichsgerade mit Folie")
 plt.plot(x_plot, linear(x_plot, *params_wo), "-", label = r"Ausgleichsgerade ohne Folie")
 
+print("Params with Foil : a=", params_w[0] , "+-", np.sqrt(np.diag(covariance_matrix_w))[0])
+print("Params with Foil : b=", params_w[1] , "+-", np.sqrt(np.diag(covariance_matrix_w))[1])
+print("Params without Foil a=", params_wo[0] , "+-", np.sqrt(np.diag(covariance_matrix_wo))[0])
+print("Params without Foil b=", params_wo[1] , "+-", np.sqrt(np.diag(covariance_matrix_wo))[1])
+
 plt.xlabel('p / mbar')
 plt.ylabel('U / V')
 plt.legend(loc='best')
@@ -42,6 +48,8 @@ plt.savefig('../content/data/plots/gerade.pdf')
 E_a = 5.4865 *10**6 * const.e
 kappa, b_wo = func.avg_loss(E_a, params_wo, covariance_matrix_wo)
 b_w = func.avg_loss(E_a, params_w, covariance_matrix_w)[1]
+
+print("Kappa: ", kappa/(10**6 * const.e))
 
 dE= kappa*(b_wo-b_w)
 print("dE = ", dE/(10**6 * const.e))
@@ -64,7 +72,7 @@ angle, hits, time  = np.genfromtxt('../content/data/tables/Txt/dstreuwinkel.txt'
 hits = unp.uarray(hits, np.sqrt(hits))
 theta=angle *np.pi/180
 
-dsdO_e = (hits/time)/(A2*dO*n*2*10**(-6) )
+dsdO_e = (hits/time)/(A_exp*dO*n*2*10**(-6) )
 dsdO_t=1/(4*np.pi*const.epsilon_0)**2*((Z*z*const.e**2)/(4*E_a))**2*1/(np.sin(theta*0.5))**4  
 
 func.diffWQ_table(angle, hits, time, dsdO_e, dsdO_t)
@@ -79,9 +87,9 @@ plt.plot(theta, noms(dsdO_e*10**24), 'bx', label = 'Wertepaare Experimentell')
 plt.errorbar(theta, noms(dsdO_e*10**24), yerr=10*stds(dsdO_e*10**24), fmt="none", capsize=3, capthick=1, ms=9, color="red", label = 'Unsicherheit mal 10')
 plt.yscale("log")
 plt.legend(loc='best')
-#func.Plot(x=theta,y=dsdO_e*10**24,xname=r'$\theta$',yname=r'Test',markername='Wertepaare Experiment',linear=False,save=False,Plot=True)
-#ST.set_ylim(0, 100)
+plt.xlabel(r'\theta/rad')
 plt.savefig('../content/data/plots/'+name+'.pdf')
+plt.ylabel(r"$\left(\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}\right)/10^{-24}m^2$")
 
 plt.cla()
 plt.clf()
@@ -96,10 +104,29 @@ mu, Hits, time = np.genfromtxt('../content/data/tables/Txt/efoliendicke.txt',unp
 N=unp.uarray(Hits,np.sqrt(Hits))
 I=N/time
 
-dsdO2m=I[0]/(A2*n*2*10**(-6)*dO)
-dsdO4m=I[1]/(A2*n*4*10**(-6)*dO)
+dsdO2m=I[0]/(A_exp*n*2*10**(-6)*dO)
+dsdO4m=I[1]/(A_exp*n*4*10**(-6)*dO)
 print("dsd02m : ", dsdO2m)
 print("dsd04m : ", dsdO4m)
 
 #Z-Abhängigkeit
-Ordnungszahl = np.array([79, 13, 83]) # Gold, Alu, Bismuth
+material, Z, d, hits, time, angle  = np.genfromtxt('../content/data/tables/Txt/fordnungszahl.txt',unpack=True, skip_header = 2)
+n_array=np.array([6.2*10**28,5.9*10**28,2.9*10**28])
+
+theta=angle * np.pi/180
+dsdO_e = (hits/time)/(A_exp*dO*d*n_array*2*10**(-6) )
+dsdO_t = 1/(4*np.pi*const.epsilon_0)**2*((Z*z*const.e**2)/(4*E_a))**2*1/(np.sin(theta*0.5))**4  
+
+plt.cla()
+plt.clf()
+name = 'atomic_number'
+plt.plot(Z, dsdO_t*10**24, 'mx', label='Wertepaare Theorie')
+plt.plot(Z, noms(dsdO_e*10**24), 'bx', label = 'Wertepaare Experimentell')
+#plt.errorbar(Z, noms(dsdO_e*10**24), yerr=100*stds(dsdO_e*10**24), fmt="none", capsize=3, capthick=1, ms=9, color="red", label = 'Unsicherheit mal 100')
+plt.xlabel(r"$Z$")
+plt.ylabel(r"$\left(\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}\right)/10^{-24}m^2$")
+plt.legend(loc='best')
+plt.savefig('../content/data/plots/'+name+'.pdf')
+
+print(dsdO_e)
+print(dsdO_t)

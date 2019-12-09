9f786b280e02873f53c33507a01869d4afcba1b3
Katti Brae <katharina.braegelmann@tu-dortmund.de>
2019-12-09 10:30:12 +0100

    germanium

---
+++ MA_FP/rutherford/content/calc.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+import uncertainties
+
+
+n = (501-15.64)/501*100
+print(n)
